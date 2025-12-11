daefdbdb4fe11a36c1d6f1e81eea338b9a24679e
Chris YU <hmyu2illinois@gmail.com>
2025-12-11 09:20:21 +0800

    Remove Wayback Machine wombat code from script.js and Google Fonts
    - Cleaned script.js by removing Wayback Machine wrapper code
    - Removed Google Fonts import from style.css
    - This should significantly improve page load speed

--- sensusbeauty_archive/js/script.js
+++ sensusbeauty_archive/js/script.js
@@ -1,13 +1,3 @@
-var _____WB$wombat$assign$function_____=function(name){return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name))||self[name];};if(!self.__WB_pmw){self.__WB_pmw=function(obj){this.__WB_source=obj;return this;}}{
-let window = _____WB$wombat$assign$function_____("window");
-let self = _____WB$wombat$assign$function_____("self");
-let document = _____WB$wombat$assign$function_____("document");
-let location = _____WB$wombat$assign$function_____("location");
-let top = _____WB$wombat$assign$function_____("top");
-let parent = _____WB$wombat$assign$function_____("parent");
-let frames = _____WB$wombat$assign$function_____("frames");
-let opens = _____WB$wombat$assign$function_____("opens");
-var popup_slider = false;
 $(document).ready(function () {
 	var isIE = document.all && !window.atob;
 
@@ -283,10 +273,3 @@ playback timings (ms):
   captures_list: 0.801
   exclusion.robots: 0.094
   exclusion.robots.policy: 0.079
-  esindex: 0.011
-  cdx.remote: 11.563
-  LoadShardBlock: 85.048 (3)
-  PetaboxLoader3.datanode: 85.839 (4)
-  PetaboxLoader3.resolve: 44.656 (2)
-  load_resource: 65.8
-*/
